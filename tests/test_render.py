d6856b3f6fd9acebfddd8222567c258beb8c0309
Aviel Yosef <ayosef@redhat.com>
2020-12-26 21:46:06 +0200

    Adding functests

---
+++ tests/test_render.py
@@ -0,0 +1,102 @@
+import requests
+
+from click.testing import CliRunner
+from jinja2_tools.cli import main as jinja
+
+TEMPLATE_URL = 'https://raw.githubusercontent.com/Avielyo10/jinja2-tools/master/examples/template.sh'
+DATA_URL = 'https://raw.githubusercontent.com/Avielyo10/jinja2-tools/master/examples/data.yaml'
+EXTRA_VARS = 'access_lists={\"al-hq-in\": [{\"action\": \"remark\", \"text\": \"Allow traffic from hq to local office\"}, {\"action\": \"permit\", \"src\": \"10.0.0.0/22\", \"dst\": \"10.100.0.0/24\"}]}'
+
+DEFAULT_MSG = """(1)
+ip access-list extended al-hq-in
+(2)
+    (3)    remark Allow traffic from hq to local office
+    (4)(2)
+    (3)    permit 10.0.0.0/22 10.100.0.0/24
+    (5)(6)
+(7)
+hello jinja!
+# All ACLs have been generated
+"""
+
+MSG_WITH_LB_TB = """(1)
+ip access-list extended al-hq-in
+  (2)
+    (3)
+    remark Allow traffic from hq to local office
+    (4)
+  (2)
+    (3)
+    permit 10.0.0.0/22 10.100.0.0/24
+    (5)
+  (6)
+(7)
+hello jinja!
+# All ACLs have been generated
+"""
+
+
+def test_simple_render():
+    runner = CliRunner()
+    yaml_result = runner.invoke(
+        jinja, ['render', '-d', 'examples/data.yaml', '-t', 'examples/template.sh'])
+    assert yaml_result.exit_code == 0
+    assert yaml_result.output == DEFAULT_MSG
+
+    json_result = runner.invoke(
+        jinja, ['render', '-d', 'examples/data.json', '-t', 'examples/template.sh'])
+    assert json_result.output == yaml_result.output
+
+
+def test_simple_render_with_tb_lb():
+    runner = CliRunner()
+    yaml_result = runner.invoke(
+        jinja, ['render', '-lb', '-tb', '-d', 'examples/data.yaml', '-t', 'examples/template.sh'])
+    assert yaml_result.exit_code == 0
+    assert yaml_result.output == MSG_WITH_LB_TB
+
+    json_result = runner.invoke(
+        jinja, ['render', '-lb', '-tb', '-d', 'examples/data.json', '-t', 'examples/template.sh'])
+    assert json_result.output == yaml_result.output
+
+
+def test_render_with_url():
+    runner = CliRunner()
+    result = runner.invoke(
+        jinja, ['render', '-d', DATA_URL, '-t', TEMPLATE_URL])
+    assert result.exit_code == 0
+    assert result.output == DEFAULT_MSG
+
+
+def test_render_with_stdin_data():
+    runner = CliRunner()
+    result = runner.invoke(
+        jinja, ['render', '-d', '-', '-t', TEMPLATE_URL], input=requests.get(DATA_URL).text)
+    assert result.exit_code == 0
+    assert result.output == DEFAULT_MSG
+
+
+def test_render_with_stdin_template():
+    runner = CliRunner()
+    result = runner.invoke(
+        jinja, ['render', '-d', DATA_URL, '-t', '-'], input=requests.get(TEMPLATE_URL).text)
+    assert result.exit_code == 0
+    assert result.output == DEFAULT_MSG
+
+
+def test_render_with_extra_vars():
+    runner = CliRunner()
+    result = runner.invoke(
+        jinja, ['render', '-d', 'examples/data.yaml', '-t', 'examples/template.sh', '-e', EXTRA_VARS])
+
+    assert result.exit_code == 0
+    assert result.output == DEFAULT_MSG
+
+
+def test_render_with_extra_vars_override():
+    runner = CliRunner()
+    result = runner.invoke(
+        jinja, ['render', '-d', 'examples/data.yaml', '-t', 'examples/template.sh', '-e', EXTRA_VARS, '-e', 'message=world'])
+
+    assert result.exit_code == 0
+    assert 'hello world!' in result.output
