9368cfb8ac7ab803520c86a774a4f712475e26f0
Aviel Yosef <ayosef@redhat.com>
2020-12-27 20:39:16 +0200

    Pylint validators.py

--- jinja2_tools/validators.py
+++ jinja2_tools/validators.py
@@ -1,9 +1,13 @@
+"""
+Validators
+"""
 import json
 import os
 import validators
 
 
 def validate_json(j):
+    """Try to parse as JSON, if it fails return it as a raw string"""
     try:
         ans = json.loads(j)
     except json.decoder.JSONDecodeError:
@@ -13,17 +17,21 @@ def validate_json(j):
 
 
 def validate_url(url) -> bool:
+    """Try to parse as URL"""
     return validators.url(url)
 
 
 def validate_path(path) -> bool:
+    """Validate that the path exist & have permisions to read from"""
     return os.path.exists(path) and \
         os.access(os.path.dirname(path), os.R_OK)
 
 
 def validate_is_file(path) -> bool:
+    """validate_path(path) and is a file"""
     return validate_path(path) and os.path.isfile(path)
 
 
 def validate_is_dir(path) -> bool:
+    """validate_path(path) and is a directory"""
     return validate_path(path) and os.path.isdir(path)
