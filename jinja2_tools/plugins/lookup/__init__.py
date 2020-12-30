6df5b87e656d1972135b5c5b8b0371a67fbfb6ca
Aviel Yosef <ayosef@redhat.com>
2020-12-30 23:04:38 +0200

    Rewrite lookup plugin & add comments

--- jinja2_tools/plugins/lookup/__init__.py
+++ jinja2_tools/plugins/lookup/__init__.py
@@ -1,3 +1,6 @@
+"""
+Lookup plugin
+"""
 import os
 import sys
 
@@ -5,21 +8,42 @@ from colors import red
 
 
 class Lookup:
+    """
+    Base class of lookup plugin
+    """
     def __init__(self, args):
-      if len(args) == 2:
-        self.op, self.args = args
-      else:
-        raise ValueError(args)
+        if len(args) == 2:
+            self.op, self.args = args
+        else:
+            raise ValueError(args)
 
 
 class LookupResolver:
     def resolve(_, lookup):
-        if lookup.op == 'env':
-            try:
-                key = os.environ[lookup.args]
-            except OSError as err:
-                print(red('[ERROR]'), err.strerror, file=sys.stderr)
-            else:
-                return key
-        else:
-            raise ValueError(lookup.op)
+        """
+        Get the right resolver and act accordingly
+        """
+        resolver = get_resolver(lookup.op)
+        return resolver(lookup)
+
+
+def get_resolver(op):
+    """
+    Get the right resolver
+    """
+    if op == 'env':
+        return _resolve_env
+    else:
+        raise ValueError(op)
+
+
+def _resolve_env(lookup):
+    """
+    Implement environment resolver
+    """
+    try:
+        key = os.environ[lookup.args]
+    except OSError as err:
+        print(red('[ERROR]'), err.strerror, file=sys.stderr)
+    else:
+        return key
