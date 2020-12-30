9b97e2eaa6047aa20929a47c2ef26f19f776949f
Aviel Yosef <ayosef@redhat.com>
2020-12-30 23:19:06 +0200

    Support lookup file

--- jinja2_tools/plugins/lookup/__init__.py
+++ jinja2_tools/plugins/lookup/__init__.py
@@ -6,14 +6,17 @@ import sys
 
 from colors import red
 
+from jinja2_tools.validators import validate_is_file
+
 
 class Lookup:
     """
     Base class of lookup plugin
     """
+
     def __init__(self, args):
         if len(args) == 2:
-            self.op, self.args = args
+            self.op, self.arg = args
         else:
             raise ValueError(args)
 
@@ -33,8 +36,20 @@ def get_resolver(op):
     """
     if op == 'env':
         return _resolve_env
+    elif op == 'file':
+        return _resolve_file
     else:
         raise ValueError(op)
+
+
+def _resolve_file(lookup):
+    """
+    Implement file resolver
+    """
+    current_pwd = os.getcwd()
+    if validate_is_file(os.path.join(current_pwd, lookup.arg)):
+        with open(lookup.arg, 'r') as in_file:
+            return in_file.read()
 
 
 def _resolve_env(lookup):
@@ -42,7 +57,7 @@ def _resolve_env(lookup):
     Implement environment resolver
     """
     try:
-        key = os.environ[lookup.args]
+        key = os.environ[lookup.arg]
     except OSError as err:
         print(red('[ERROR]'), err.strerror, file=sys.stderr)
     else:
