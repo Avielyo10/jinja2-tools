cc058ce9f05c36aab64e8421cf7298cef94ca6d8
Aviel Yosef <avielyo10@gmail.com>
2024-03-06 20:05:46 +0200

    v1.0.7 -> v1.0.8

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r", encoding="utf-8") as readme_file:
 
 setuptools.setup(
     name="jinja2-tools",
-    version="1.0.7",
+    version="1.0.8",
     author="Aviel Yosef",
     author_email="Avielyo10@gmail.com",
     description="Use Jinja2 templates via cli",
